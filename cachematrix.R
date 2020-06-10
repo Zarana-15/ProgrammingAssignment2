001e6a65a492c35c6409066db9d932b901c78b07
Zarana-15 <zaranadesai16@gmail.com>
2020-06-10 14:30:29 +0530

    Programming Assignment 2 submission

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  ##This function creates a 'special'matrix to then obtain or cache its inverse
+  ## The <<- operator can be used to assign a value to an object in an environment that is different from the current environment
+  Inv<- NULL
+  set <- function(y){
+    x <<- y
+    Inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(Solve) Inv <<- Solve
+  getInverse <- function() Inv
+  newMat<<-list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  newMat
+  } 
 
+cacheSolve<- function(z=newMat, ...) {
+  ## Returns a matrix that is the inverse of 'x' from makeCacheMatrix
+  Inv <- z$getInverse()
+  if(!is.null(Inv)){
+    message("getting cached data")
+    return(Inv)
+  }
+  data <- z$get()
+  Inv <- solve(data)
+  z$setInverse(Inv)
+  Inv      
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    
